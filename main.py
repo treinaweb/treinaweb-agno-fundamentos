211514c9a2b402356d1d51bd1081e4c4ca6d7d3b
Cleyson Lima <cleysonph@gmail.com>
2026-06-03 16:56:18 -0300

    03:03 - Adicionando tools no nosso agente

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from agno.agent import Agent
 from agno.models.openai import OpenAIResponses
+from agno.tools.hackernews import HackerNewsTools
 from dotenv import load_dotenv
 
 load_dotenv()
 
 agent = Agent(
     model=OpenAIResponses(id="gpt-4.1-mini"),
+    tools=[HackerNewsTools()],
 )
 
-agent.print_response("Me explique o que é uma LLM?")
-
-# result = agent.run("Me explique o que é uma LLM?")
-# print(result)
+agent.print_response("Quais os tópicos mais discutidos no Hacker News hoje?")
