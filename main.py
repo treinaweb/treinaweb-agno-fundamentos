594a159992b331290c99399555a63e11d16b2a3a
Cleyson Lima <cleysonph@gmail.com>
2026-06-03 23:43:27 -0300

    03:09 - Trabalhando com mémorias de usuário

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ load_dotenv()
 agent = Agent(
     model="openai:gpt-4.1-mini",
     db=SqliteDb("agent.db"),
-    add_history_to_context=True,
+    enable_agentic_memory=True,
 )
 
-agent.print_response(
-    "Qual o meu nome?",
-    session_id="0d581f97fd5491f900971d7704b24d42360f73cc7887fbe5ccae26fa1dcad280",
-)
+memories = agent.get_user_memories(user_id="cleyson@mail.com")
+
+for memory in memories:
+    print(memory.memory)
