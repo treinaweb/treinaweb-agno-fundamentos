847019a9701c96df6789bd9831184792c274319c
Cleyson Lima <cleysonph@gmail.com>
2026-06-03 23:13:48 -0300

    03:08 - Enviando hisotrico de mensagem para o agente

--- main.py
+++ main.py
@@ -1,60 +1,17 @@
-from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from agno.agent import Agent
-
-from job_info import JOB_INFO
+from agno.db.sqlite import SqliteDb
 
 load_dotenv()
 
 
-class JobData(BaseModel):
-    title: str = Field(description="Título da vaga de emprego")
-    company: str | None = Field(description="Nome da empresa, se disponível")
-    location: str | None = Field(description="Localização da vaga, se disponível")
-    description: str = Field(description="Descrição detalhada da vaga de emprego")
-    requirements: list[str] | None = Field(
-        description="Lista de requisitos para a vaga de emprego"
-    )
-    benefits: list[str] | None = Field(
-        description="Lista de benefícios oferecidos pela vaga de emprego"
-    )
-    is_remote: bool = Field(description="Indica se a vaga é para trabalho remoto")
-
-
-class JobAnalysis(BaseModel):
-    seniority: str = Field(
-        description="Nível de senioridade da vaga (ex: Júnior, Pleno, Sênior)"
-    )
-    technologies: list[str] = Field(
-        description="Lista de tecnologias mencionadas na vaga"
-    )
-    english_required: bool = Field(
-        description="Indica se o conhecimento de inglês é necessário para a vaga"
-    )
-    summary: str = Field(description="Resumo conciso da vaga de emprego")
-
-
-job_parser_agent = Agent(
-    name="JobParserAgent",
-    description="Agente para extrair informações de vagas de emprego a partir de descrições textuais",
-    output_schema=JobData,
+agent = Agent(
     model="openai:gpt-4.1-mini",
+    db=SqliteDb("agent.db"),
+    add_history_to_context=True,
 )
 
-job_analysis_agent = Agent(
-    name="JobAnalysisAgent",
-    description="Agente para analisar as informações extraídas de uma vaga de emprego e fornecer insights adicionais",
-    output_schema=JobAnalysis,
-    input_schema=JobData,
-    model="openai:gpt-4.1-mini",
+agent.print_response(
+    "Qual o meu nome?",
+    session_id="0d581f97fd5491f900971d7704b24d42360f73cc7887fbe5ccae26fa1dcad280",
 )
-
-job_parser_result = job_parser_agent.run(JOB_INFO)
-
-job_analysis_result = job_analysis_agent.run(job_parser_result.content)
-
-print("=== Job Data ===")
-print(job_parser_result.content.model_dump_json(indent=2))
-
-print("\n=== Job Analysis ===")
-print(job_analysis_result.content.model_dump_json(indent=2))
